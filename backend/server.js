9ae23b9cd62b265e96b344e020a0a66e911f36e2
Smit018 <smitagrawal789@gmail.com>
2024-08-25 13:22:42 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,11 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors());
+app.use(cors({
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
